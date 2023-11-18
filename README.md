b6182cfc5e326418167dd5835f2b33a15ea75530
Arjun Sasikumar <itsarjn@gmail.com>
2023-11-18 10:56:31 +0530

    NFT mint dApp

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple NFT Mint dApp
+
+📱 Open https://mint-nfts-three.vercel.app/ to see the app.
+
+---
+
+Thank you !
---
+++ packages/nextjs/pages/index.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import type { NextPage } from "next";
+import { MetaHeader } from "~~/components/MetaHeader";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <MetaHeader />
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5 w-[90%] md:w-[75%]">
+          <h1 className="text-center mb-6">
+            <span className="block text-4xl font-bold">Mint NFT</span>
+          </h1>
+          <div className="flex flex-col items-center justify-center">
+            <Image
+              src="/hero.png"
+              width="727"
+              height="231"
+              alt="challenge banner"
+              className="rounded-xl border-4 border-primary"
+            />
+            <div className="max-w-3xl">
+              <p className="text-center text-lg">
+                This is a very simple dApp to mint NFTs. This is built using a template from{" "}
+                <a href="https://speedrunethereum.com/" target="_blank" rel="noreferrer" className="underline">
+                  SpeedRunEthereum.com
+                </a>{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
